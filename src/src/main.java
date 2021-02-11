591af9b1d871506ed95bac91998dbd56dec473c1
Sebastian Pichler <s.pichler100795@gmail.com>
2021-02-11 14:40:34 +0100

    Add Exercise

---
+++ src/src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public static void main(String[] args){
+
+        String strings=" Hello World! ";
+
+        System.out.println(strings);
+        System.out.println(strings.toLowerCase());
+        System.out.println(strings.toUpperCase());
+        System.out.println(strings.replaceFirst("World","Codersbay"));
+        System.out.println(strings.replaceAll(" ",""));
+        System.out.println((strings+"\n").repeat(15));
+
+         }
+}
