4b832f5d500d47855e4839d120bc0db95e368793
Sebastian100795 <78736851+Sebastian100795@users.noreply.github.com>
2021-02-15 09:05:53 +0100

    Update main.java

--- src/src/main.java
+++ src/src/main.java
@@ -8,7 +8,7 @@ public class main {
         System.out.println(strings.toLowerCase());
         System.out.println(strings.toUpperCase());
         System.out.println(strings.replaceFirst("World","Codersbay"));
-        System.out.println(strings.substring(1, strings.length() - 1));
+        System.out.println(strings.stripLeading());
         System.out.println((strings+"\n").repeat(15));
          }
 }
